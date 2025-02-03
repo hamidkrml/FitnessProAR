6b026e26ca56590ab3c4bc63dd96076379db93c2
hamidkrml <karimlihamid4@gmail.com>
2025-02-03 18:04:39 +0300

    güncellendi

---
+++ BitirmeProjesi/Screen/Home/Spor/tab/spor.swift
@@ -0,0 +1,131 @@
+//
+//  spor.swift
+//  BitirmeProjesi
+//
+//  Created by hamid on 03.02.25.
+//
+
+import SwiftUI
+
+struct spor: View {
+    let columns = [
+        GridItem(.flexible())
+    ]
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                VStack{
+                    //yatay hizalandirmak icin
+                    ScrollView(.horizontal,showsIndicators: false){
+                        HStack(spacing:25){
+                            NavigationLink(destination: KayitOl()){
+                                Image("fitness")
+                                    .resizable()
+                                    .frame(width: 300,height: 200)
+                                    .cornerRadius(10)
+                            }
+                            NavigationLink(destination: KayitOl()){
+                                Image("fitness")
+                                    .resizable()
+                                    .frame(width: 350,height: 200)
+                                    .cornerRadius(10)
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                    Divider()
+                    
+                    LazyVGrid(columns: columns, spacing: 25) {
+                        
+                        
+                        NavigationLink(destination: Squatvid()) {
+                            HStack(spacing: 50) {
+                                Image("squat")
+                                    .resizable()
+                                    .frame(width: 110, height: 100)
+                                Text("Squat ")
+                                    .font(.title2)
+                                    .foregroundColor(.black)
+                                
+                            }
+                            .maxLeft
+                        }
+                        
+                        NavigationLink(destination: Bicepvid()) {
+                            HStack(spacing: 50  ) {
+                                
+                                Image("bicep")
+                                    .resizable()
+                                    .frame(width: 110, height: 100)
+                                Text("Biceps ")
+                                    .font(.title2)
+                                    .foregroundColor(.black)
+                            }
+                            .maxLeft
+                        }
+                        NavigationLink(destination: Standingvid()) {
+                            HStack(spacing: 50) {
+                                
+                                Image("lateral")
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                                Text("lateral")
+                                    .font(.title2)
+                                    .foregroundColor(.black)
+                            }
+                            .maxLeft
+                        }
+                        NavigationLink(destination: Pressvid()) {
+                            HStack(spacing: 50) {
+                                
+                                Image("dumbel")
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                                Text("dumbelPres")
+                                    .font(.title2)
+                                    .foregroundColor(.black)
+                            }
+                            .maxLeft
+                        }
+                        
+                        NavigationLink(destination: Lsagvid()) {
+                            HStack(spacing: 50) {
+                                
+                                Image("sag")
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                                Text("Lungess")
+                                    .font(.title2)
+                                    .foregroundColor(.black)
+                            }
+                            .maxLeft
+                        }
+                        
+                        NavigationLink(destination: Lsolvid()) {
+                            HStack(spacing: 50) {
+                                
+                                Image("sol")
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                                
+                                Text("Lungess")
+                                    .font(.title2)
+                                    .foregroundColor(.black)
+                                
+                                
+                            }.maxLeft
+                            
+                                .cornerRadius(10)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("sporlarimiz")
+        }.navHide
+    }
+    
+    
+}
+#Preview {
+    spor()
+}
