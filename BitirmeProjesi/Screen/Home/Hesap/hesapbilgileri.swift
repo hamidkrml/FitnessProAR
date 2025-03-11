999eb837f789f24c70d2fc68873539dc5908d241
hamidkrml <karimlihamid4@gmail.com>
2025-03-11 18:20:21 +0300

    hesap sayfasi

--- BitirmeProjesi/Screen/Home/Hesap/hesapbilgileri.swift
+++ BitirmeProjesi/Screen/Home/Hesap/hesapbilgileri.swift
@@ -7,24 +7,61 @@
 
 import Foundation
 import SwiftUI
+import PhotosUI
+
 
 struct hesapbilgileri :View {
     var body: some View {
-
-        VStack {
-            Text("kullancilarin bilgileri ve suan kadar yapdiklari işlemleri gösterilecek ve beden kitle indeksi ve")
-                .font(.customfont(font: .Bold, fontSize: 22))
-        }
-            
-            
-                .font(.customfont(font: .Bold, fontSize: 22))
-            
-                .frame(maxWidth: .infinity,maxHeight: .infinity)
-                .background(
-                    ExtractedView.shared
-                )
         
-                .preferredColorScheme(.dark)
+        @State var selectedPhoto: PhotosPickerItem? = nil
+        @State var profileImage: Image? = nil
+        
+        NavigationView {
+            ZStack {
+                ExtractedView.shared
+                    .ignoresSafeArea()
+                
+                VStack(spacing: 30) {
+                    HStack{
+                    if let profileImage = profileImage {
+                                    profileImage
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 120, height: 120)
+                                        .clipShape(Circle())
+                                        .shadow(radius: 5)
+                    } else {
+                        
+                        Image(systemName: "person.crop.circle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 120, height: 120)
+                            .foregroundColor(.gray)
+                        
+                        PhotosPicker("Fotoğraf Seç", selection: $selectedPhoto, matching: .images)
+                            .buttonStyle(.bordered)
+                            .padding(.top, 10)
+                        
+                        
+                    }
+                        
+                    }
+                    .maxLeft
+                    .top15
+                    .horizontal8
+                    
+                    VStack{
+                        Text("ghfkghgh")
+                        
+                    }
+                    Spacer()
+                }
+                
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+           
+        }
+        .preferredColorScheme(.dark)
     }
         
 }
